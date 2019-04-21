40b17a1d6c5624810811b27ca191c5b848f65d58
杨健 <yj021@foxmail.com>
2019-04-21 23:47:35 +0800

    Update db_config_dev.py

--- config/db_config_dev.py
+++ config/db_config_dev.py
@@ -3,8 +3,8 @@
 
 #开发环境的数据库配置
 
-DB_HOST = 'localhost'#数据库地址
-DB_USER_NAME = 'root'#数据库连接用户名
+DB_HOST = 'localhost'   #数据库地址
+DB_USER_NAME = 'root'   #数据库连接用户名
 DB_PASSWORD = ''        #数据库密码
 DB_NAME = 'db_data'     #数据库名称
 DB_PORT = 3306          #数据库端口号
