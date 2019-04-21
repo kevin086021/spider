766e312a6f3e224732e59be7075f347e880b35da
杨健 <yj021@foxmail.com>
2019-04-21 23:48:39 +0800

    Update db_config_pro.py

--- config/db_config_pro.py
+++ config/db_config_pro.py
@@ -3,9 +3,9 @@
 
 #生产环境的数据库配置
 
-DB_HOST = 'online_host_address'					#数据库地址
-DB_USER_NAME = 'online_user_name'				#数据库连接用户名
-DB_PASSWORD = 'online_user_password'			#数据库密码
-DB_NAME = 'online_db_name'						#数据库名称
-DB_PORT = 3306									#数据库端口号
-DB_CHARSET = 'utf8'								#数据库编码方式
+DB_HOST = 'online_host_address'         #数据库地址
+DB_USER_NAME = 'online_user_name'       #数据库连接用户名
+DB_PASSWORD = 'online_user_password'    #数据库密码
+DB_NAME = 'online_db_name'              #数据库名称
+DB_PORT = 3306                          #数据库端口号
+DB_CHARSET = 'utf8'                     #数据库编码方式
